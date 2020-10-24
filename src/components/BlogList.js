0c22695c439a34f9010bba1b13aba7b674c2e873
Gabeduardo <gemcir13@gmail.com>
2020-10-24 00:43:44 -0400

    Dynamic page creation template done for individual post
    creacion de gatsby-node para creacion de nodos, es decir los slugs para
    generar dinamicamente las paginas

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -16,6 +16,9 @@ export default function BlogList() {
                         title
                         date(formatString: "MMMM D, YYYY")
                     }
+                    fields {
+                        slug
+                    }
                     excerpt 
                 }
             }
@@ -30,6 +33,7 @@ return (
       {data.allMarkdownRemark.edges.map(edge => (
         <BlogPost
           key={edge.node.id}
+          slug={edge.node.fields.slug}
           title={edge.node.frontmatter.title}
           date={edge.node.frontmatter.date}
           excerpt={edge.node.excerpt} />
